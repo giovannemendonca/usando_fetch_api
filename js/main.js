b0081ae9116051f67a9cf141f68813f45a4630fa
Giovanne Mendonca <giovannemendonca@outlook.com>
2022-07-08 21:21:04 -0300

    criando html e js para buscar informações na Api e tratalas

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const url = "https://jsonplaceholder..typicode.com/users"
+
+fetch(url)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((erro) => {
+        console.log(`Opa, aconteceu um erro: \n${erro} `);
+    })
